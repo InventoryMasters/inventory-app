1d6687438ed67f91a2785815d1011e6f7344e7eb
meih15 <meihuang8328@gmail.com>
2024-05-21 17:44:11 -0400

    define category schema

--- server/models/Category.js
+++ server/models/Category.js
@@ -2,6 +2,11 @@ const { DataTypes } = require('sequelize');
 const { sequelize } = require('../db');
 
 const Category = sequelize.define('category', {
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4
+    },
     name: {
         type: DataTypes.STRING,
         allowNull: false
