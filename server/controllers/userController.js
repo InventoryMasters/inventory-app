e075523b93377fa783892a8481640faa149d5986
Bridget Galang <galanbri@gmail.com>
2024-05-21 15:45:40 -0700

    add createUser route handler and add guards

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,14 @@
 const { User } = require('../models/index');
 
 const getAllUsers = async (req, res) => {
-    try { 
+    try {
         const users = await User.findAll();
-        if (users) {
+        if (!users) {
+            res.status(404).send("Error fetching users: Users not found");
+        } else {
             res.status(200).json(users);
         }
-    } catch(error) {
+    } catch (error) {
         res.status(500).send(error);
     }
 }
@@ -15,8 +17,25 @@ const getUser = async (req, res) => {
     try {
         const userId = req.params.userId
         const user = await User.findByPk(userId);
-        if (user) {
+        if (!user) {
+            res.status(404).send(`Error fetching user: User with ID ${userId} not found`);
+        } else {
             res.status(200).json(user);
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+const createUser = async (req, res) => {
+    try {
+        const userInfo = req.body;
+        await User.create(userInfo);
+        const newUser = await User.findOne({ where: { email: userInfo.email } });
+        if (!newUser) {
+            res.status(500).json({ error: 'Error creating user '});
+        } else {
+            res.status(200).json(newUser);
         }
     } catch (error) {
         res.status(500).send(error);
@@ -28,8 +47,12 @@ const updateUser = async (req, res) => {
         const newInfo = req.body;
         const userId = req.params.userId;
         const user = User.findByPk(userId);
-        await user.update(newInfo);
-        res.status(200).send("User updated!");
+        if (!user) {
+            res.status(404).send(`Error updating user: User with ID ${userId} not found`);
+        } else {
+            await user.update(newInfo);
+            res.status(200).json(user);
+        }
     } catch (error) {
         res.status(500).send(error);
     }
@@ -39,7 +62,9 @@ const deleteUser = async (req, res) => {
     try {
         const userId = req.params.userId;
         const user = User.findByPk(userId);
-        if (user) {
+        if (!user) {
+            res.status(404).send(`Error deleting user: User with ID ${userId} not found`);
+        } else {
             await user.destroy();
             res.status(200).send("User removed!");
         }
@@ -51,6 +76,7 @@ const deleteUser = async (req, res) => {
 module.exports = {
     getAllUsers,
     getUser,
+    createUser,
     updateUser,
     deleteUser
 }
