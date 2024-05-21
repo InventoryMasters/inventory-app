4da2245cb1b17878a027a19beaf7e1fd90c94112
Bridget Galang <galanbri@gmail.com>
2024-05-21 12:07:06 -0700

    update imports

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const Router = express.Router();
 
+const { getAllUsers, getUser, updateUser, deleteUser } = require('../controllers/userController');
+
 Router.get('/users', getAllUsers);
 Router.get('/users/:userId', getUser);
 Router.put('/users/:userId', updateUser);
