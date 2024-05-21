52b8a17a4d0b78ad09006a311bc2ad9beeb7b25b
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-21 14:20:52 -0400

    add category mock data

---
+++ server/mock-data/mockCategory.js
@@ -0,0 +1,96 @@
+const mockCategory = [
+  {
+    id: '4239f2ff-0993-48c3-acb2-81d601d4d780',
+    name: 'Asphalt Paving',
+    createdAt: '1/7/2024',
+    updatedAt: '8/14/2023',
+  },
+  {
+    id: '7195483a-a3a8-43ee-abf4-6a5012a98327',
+    name: 'Prefabricated Aluminum Metal Canopies',
+    createdAt: '10/31/2023',
+    updatedAt: '2/8/2024',
+  },
+  {
+    id: '88c58cb1-ee94-4819-a256-bf0d9b9e6229',
+    name: 'Sitework & Site Utilities',
+    createdAt: '5/5/2024',
+    updatedAt: '6/16/2023',
+  },
+  {
+    id: '9b3794cc-cc25-43c7-9871-b6940efe3dde',
+    name: 'Framing (Steel)',
+    createdAt: '12/23/2023',
+    updatedAt: '8/6/2023',
+  },
+  {
+    id: '70b40307-4fa4-4b7c-a66b-a6895d84fc86',
+    name: 'Structural and Misc Steel (Fabrication)',
+    createdAt: '10/8/2023',
+    updatedAt: '1/22/2024',
+  },
+  {
+    id: 'aea2de26-f754-477b-b3ea-6548d8359c13',
+    name: 'RF Shielding',
+    createdAt: '6/25/2023',
+    updatedAt: '12/17/2023',
+  },
+  {
+    id: 'aff9da47-61b7-4806-84b9-6a602cf150b7',
+    name: 'Asphalt Paving',
+    createdAt: '1/7/2024',
+    updatedAt: '2/26/2024',
+  },
+  {
+    id: '71ee5f29-e824-41ce-bc1f-3e8561f51667',
+    name: 'Construction Clean and Final Clean',
+    createdAt: '3/26/2024',
+    updatedAt: '8/5/2023',
+  },
+  {
+    id: '3589c9f7-9a56-457b-bc4b-d9f0c4131308',
+    name: 'Doors, Frames & Hardware',
+    createdAt: '4/27/2024',
+    updatedAt: '4/15/2024',
+  },
+  {
+    id: 'b472b00a-bbef-4a1d-a746-5f7d2126e811',
+    name: 'RF Shielding',
+    createdAt: '1/14/2024',
+    updatedAt: '4/29/2024',
+  },
+  {
+    id: 'a09d32d9-175b-424f-9e61-33ff46e95525',
+    name: 'Wall Protection',
+    createdAt: '10/16/2023',
+    updatedAt: '11/13/2023',
+  },
+  {
+    id: 'f7da6b20-cf58-4ea5-9831-98e22616865d',
+    name: 'Prefabricated Aluminum Metal Canopies',
+    createdAt: '5/28/2023',
+    updatedAt: '10/28/2023',
+  },
+  {
+    id: '2fccf62c-f108-4f0f-88c2-ad8fb0e59a7b',
+    name: 'Masonry',
+    createdAt: '8/23/2023',
+    updatedAt: '6/5/2023',
+  },
+  {
+    id: 'ee562c02-5711-4d5d-aaac-4dbb195a851a',
+    name: 'Epoxy Flooring',
+    createdAt: '12/10/2023',
+    updatedAt: '9/24/2023',
+  },
+  {
+    id: '88b32bc6-ce2f-4ca8-b543-e209b9243d01',
+    name: 'Retaining Wall and Brick Pavers',
+    createdAt: '8/14/2023',
+    updatedAt: '12/22/2023',
+  },
+];
+
+
+
+module.exports = mockCategory
