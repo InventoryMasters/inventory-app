9b82419543e886f83b84cce26323fa8318e25f09
Bridget Galang <galanbri@gmail.com>
2024-05-21 16:03:10 -0700

    add createUser import and post route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const Router = express.Router();
 
-const { getAllUsers, getUser, updateUser, deleteUser } = require('../controllers/userController');
+const { getAllUsers, getUser, createUser, updateUser, deleteUser } = require('../controllers/userController');
 
 Router.get('/users', getAllUsers);
 Router.get('/users/:userId', getUser);
+Router.post('/users', createUser);
 Router.put('/users/:userId', updateUser);
 Router.delete('/users/:userId', deleteUser);
 
