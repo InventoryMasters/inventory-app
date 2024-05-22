ae38b17d2bd9ad74eef42b0bee2ca0b2807b5926
Stephanie <steph.frias01@me.com>
2024-05-22 13:21:31 -0400

    Implemented server-side validation for POST and PUT item routes

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -26,7 +26,46 @@ const getItem = async (req, res) => {
         res.status(500).send(error);
     }
 }
+const createItem = async (req, res) => {
+    try {
+        const itemInfo = req.body;
+        const newItem = await Item.create(itemInfo);
+        res.status(201).json(newItem);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
 
+const updateItem = async (req, res) => {
+    try {
+        const itemId = req.params.itemId;
+        const newInfo = req.body;
+        const item = await Item.findByPk(itemId);
+        if (!item) {
+            res.status(404).send(`Error updating item: Item with ID ${itemId} not found`);
+        } else {
+            await item.update(newInfo);
+            res.status(200).json(item);
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+const deleteItem = async (req, res) => {
+    try {
+        const itemId = req.params.itemId;
+        const item = await Item.findByPk(itemId);
+        if (!item) {
+            res.status(404).send(`Error deleting item: Item with ID ${itemId} not found`);
+        } else {
+            await item.destroy();
+            res.status(200).send("Item removed!");
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
 module.exports = {
     getAllItems,
     getItem,
