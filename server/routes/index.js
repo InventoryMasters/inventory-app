945345029a0fffb34339317aea670a6609719dbc
Stephanie <steph.frias01@me.com>
2024-05-22 14:44:16 -0400

    Resolve merge conflict in server/routes/index.js again

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,8 @@ const router = express.Router();
 const userRoutes = require('./userRoutes');
 const itemRoutes = require('./itemRoutes');
 
+
 router.use(userRoutes);
 router.use(itemRoutes);
-
-router.use('/users', require('./userRoutes'));
 
 module.exports = router;
