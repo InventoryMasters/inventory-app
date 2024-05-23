15dde95b7fb6f0b7a9be7f02f4e47730c9e07f81
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-23 17:48:11 -0400

    render all prods

---
+++ public/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import apiURL from '../api';
+import AllProducts from './AllProducts';
+
+export const App = () => {
+
+
+	return (
+		<AllProducts/>
+	)
+}
