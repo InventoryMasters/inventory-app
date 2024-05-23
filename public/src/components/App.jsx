f0296a55a58c65ecaa44bc6ab2e33a6b54dc58c4
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-23 17:57:21 -0400

    add route to products

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import apiURL from '../api';
 import AllProducts from './AllProducts';
+import Navbar from './Navbar';
 
 export const App = () => {
-
-
-	return (
-		<AllProducts/>
-	)
-}
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/products' element={<AllProducts />} />
+      </Routes>
+    </>
+  );
+};
