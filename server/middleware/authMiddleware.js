50073476f671e26461cae16b95fc919c5ec0e1c8
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-23 14:57:05 -0400

    add adminAccessRequired middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,29 +1,35 @@
 const { User } = require('../models/index');
-// const jwt = require('jsonwebtoken')
 
 async function requireToken(req, res, next) {
   try {
     const token = req.headers.authorization;
 
-    console.log({ 'TOKEN FROM MIDDLEWARE': token });
-
-    // console.log({'payload log form middleware': token.payload})
     if (!token)
       return res.status(404).json({ error: 'Must be logged in to access' });
 
-    // const decoded = jwt.verify(token.split(' ')[1], SECRET);
-
-    // console.log({ 'payload log form middleware': decoded });
     const user = await User.verifyByToken(token);
-    console.log({ user });
     req.user = user;
 
-    console.log({ REQ: req.user });
-    console.log(' WE GOT HERE');
     next();
   } catch (e) {
     next(e);
   }
 }
 
-module.exports = { requireToken };
+async function adminAccessRequired(req, res, next) {
+  try {
+
+    const token = req.headers.authorization;
+    if (!token)
+      return res.status(404).json({ error: 'Must be logged in to access' });
+
+      const adminUser = await User.verifyByToken(token)
+      req.user = adminUser
+
+    next();
+  } catch (e) {
+    next(e);
+  }
+}
+
+module.exports = { requireToken, adminAccessRequired };
