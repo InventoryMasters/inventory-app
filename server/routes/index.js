7bc337de29776e2441432b62a5f297c1579d039c
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-23 15:11:21 -0400

    add auth router

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const userRoutes = require('./userRoutes');
 const itemRoutes = require('./itemRoutes');
 const categoryRoutes = require('./categoryRoutes');
+const authRoutes = require('./authRoutes')
 
 
 router.use('/items', itemRoutes); 
 router.use('/users', userRoutes); 
 router.use('/categories', categoryRoutes);
+router.use('/auth', authRoutes)
 
 
 
