da5e408e7758b48fdb262220948a1f73894a3d45
Bridget Galang <galanbri@gmail.com>
2024-05-24 13:12:42 -0700

    add banner image

--- public/src/components/products/allProducts/AllProducts.jsx
+++ public/src/components/products/allProducts/AllProducts.jsx
@@ -21,21 +21,29 @@ export default function AllProducts() {
 
   return (
     <section className='pt-20'>
-      <h1 className='font-abril '>All Products</h1>
-      <div className=''>
-        {Array.isArray(products) && products.length ? (
-          products.map((product) => (
-            <div key={product.id}>
-              <img className='w-[350px] h-auto' src={product.imageUrl} alt={product.name} />
-              <h2>{product.name}</h2>
-              <p>{product.description}</p>
-              <p>Price: ${product.price}</p>
-              <p>Quantity: {product.qty}</p>
-            </div>
-          )) 
-        ) : (
-          <p>No products available</p>
-        )}
+      <div className='container mx-auto'>
+        <div className='w-full banner-container'>
+          <img src='https://images.pexels.com/photos/7500307/pexels-photo-7500307.jpeg' alt='Banner Image' className='w-full object-cover h-40'/>
+          <h1 className='font-abril '>All Products</h1>
+        </div>
+        <div className='grid grid-cols-3 gap-4 mt-8'>
+          {Array.isArray(products) && products.length ? (
+            products.map((product) => (
+              <div key={product.id}>
+                <img className='w-[350px] h-auto' src={product.imageUrl} alt={product.name} />
+                <h2>{product.name}</h2>
+                <p>{product.description}</p>
+                <p>Price: ${product.price}</p>
+                <p>Quantity: {product.qty}</p>
+              </div>
+            ))
+          ) : (
+            <p>No products available</p>
+          )}
+        </div>
+
+        {/* </div>
+      <div className=''> */}
       </div>
     </section>
   );
