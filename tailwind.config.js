fd5542da4d1e6252c9a6267acc1d7cde1feaf38c
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-24 11:21:37 -0400

    add fonts, weight, colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
+  content: ['public/**/*.{html,js,jsx}'],
+
   theme: {
-    extend: {},
+    fontFamily: {
+      encode: ['Encode Sans Condensed', 'sans-serif'],
+      abril: ['Abril Fatface', 'serif'],
+      epilogue: ['Epilogue', 'sans-serif'],
+    },
+
+    fontWeight: {
+      thin: 100,
+      'extra-light': 200,
+      light: 300,
+      regular: 400,
+      medium: 500,
+      'semi-bold': 600,
+      bold: 700,
+      'extra-bold': 800,
+      black: 900,
+    },
+
+    extend: {
+      colors: {
+        'primary-logo': '#CCB099',
+        'primary-light-gray': '#616161',
+        'primary-dark-gray': '#323232',
+        'primary-backdrop': '#F6F8F6',
+        'primary-popup': '#D6CBC4',
+      },
+    },
   },
+
   plugins: [],
 };
-
