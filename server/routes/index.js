da2da49eed65a3a32e190d8dfe557a3f59338ec5
Anastasia Lukavsky <92340133+anastasialukavsky@users.noreply.github.com>
2024-05-24 13:22:22 -0400

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 const userRoutes = require('./userRoutes');
 const itemRoutes = require('./itemRoutes');
 const categoryRoutes = require('./categoryRoutes');
@@ -14,6 +13,5 @@ router.use('/users', userRoutes);
 router.use('/categories', categoryRoutes);
 router.use('/auth', authRoutes);
 router.use('/admin', adminAccessRequired, adminRoutes);
-
 
 module.exports = router;
