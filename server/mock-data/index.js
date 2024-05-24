e585082683da545110f2bf56d463366b92f09d8e
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-24 14:53:01 -0400

    import real items

--- server/mock-data/index.js
+++ server/mock-data/index.js
@@ -1,5 +1,6 @@
 const mockUser = require('./mockUser');
 const mockItem = require('./mockItem');
 const mockCategory = require('./mockCategory');
+const items = require('./itemData')
 
-module.exports = { mockUser, mockItem , mockCategory };
+module.exports = { mockUser, mockItem , mockCategory, items };
