a0af0937b92202545850800b55bb309be1441bec
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-24 13:07:44 -0400

    move

--- public/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Navbar() {
-  return (
-    <div className='text-5xl underline font-abril text-center text-primary-logo border border-black h-20 max-w-[100vw] bg-red-500'>
-      NAVBAR
-    </div>
-  );
-}
