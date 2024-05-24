5465559d041dbd18930544a2b02e2a28c36a22a6
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-24 16:34:03 -0400

    fix api

--- public/src/components/products/singleProduct/SingleProduct.jsx
+++ public/src/components/products/singleProduct/SingleProduct.jsx
@@ -1,17 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import apiURL from '../../../api'
 
 
 export default function SingleProduct() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
 
+  console.log({id})
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const response = await axios.get(`/api/items/${id}`);
+        const response = await axios.get(`${apiURL}/items/${id}`);
         setProduct(response.data);
+        console.log({product})
       } catch (error) {
         console.error('Error fetching product:', error);
       }
@@ -25,12 +28,12 @@ export default function SingleProduct() {
   }
 
   return (
-    <div>
+    <section className='bg-slate-500 flex '>
       <h1>{product.name}</h1>
-      <img src={product.imageUrl} alt={product.name} />
+      <img className='w-[300px]' src={product.imageUrl} alt={product.name} />
       <p>{product.description}</p>
       <p>Price: ${product.price}</p>
       <p>Quantity: {product.qty}</p>
-    </div>
+    </section>
   );
 }
