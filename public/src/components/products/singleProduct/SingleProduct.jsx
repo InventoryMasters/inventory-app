772bf7dd1126b8431e79e70509ba0f58cb211e47
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-24 23:09:15 -0400

    wxtract SP into separate component

--- public/src/components/products/singleProduct/SingleProduct.jsx
+++ public/src/components/products/singleProduct/SingleProduct.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
 import apiURL from '../../../api';
-import shevronLeft from '../../../../assets/icons/shevron-left.svg'
+import shevronLeft from '../../../../assets/icons/shevron-left.svg';
+import ProductCard from './productCard';
 
 export default function SingleProduct() {
   const { id } = useParams();
@@ -30,33 +31,20 @@ export default function SingleProduct() {
   return (
     <main className=' flex w-[100dvw] h-[100dvh] justify-center pt-[5rem]'>
       <section className='bg-primary-backdrop rounded-t-3xl'>
-        <div className='flex max-w-[80vw] gap-36 pt-36 bg-primary-backdrop rounded-t-full px-36'>
-          <div className='w-5/12 h-auto'>
-            <img src={product.imageUrl} alt={product.name} />
-          </div>
-          <div className='text-center font-encode pt-10 w-7/12 bg-transparent'>
-            <h1 className='bg-transparent text-[1.6rem] font-light uppercase'>
-              {product.name}
-            </h1>
-            <p className='lowercase font-epilogue italic font-thin text-sm pt-2 bg-transparent'>
-              {product.category.name}
-            </p>
-            <article className='bg-transparent font-extra-light pt-6'>
-              {product.description}
-            </article>
-            <p className='bg-transparent uppercase pt-6 font-light'>
-              qty in stock: {product.qty}
-            </p>
-            <p className='bg-transparent font-thin pt-1 uppercase'>
-              Price: ${product.price}
-            </p>
-          </div>
-        </div>
-
-        <section className='bg-transparent font-encode font-thin flex justify-start items-center gap-6 mt-36 ml-36 border border-primary-dark-gray w-fit px-12 py-2 rounded-full'> 
-        <img className='bg-transparent h-4' src={shevronLeft} alt="Back to products" />
-        <Link className='bg-transparent text-sm' to={'/products'}>back to products</Link>
-        </section>
+        <ProductCard product={product}/>
+        <Link
+          to='/products'
+          className='bg-transparent text-sm flex gap-7 items-start ml-20 justify-start'
+        >
+          <section className='flex flex-row-reverse gap-10 items-center bg-transparent font-encode font-thin mt-36 border border-primary-dark-gray w-fit px-12 py-2 rounded-full'>
+            back to products
+            <img
+              className='bg-transparent h-4'
+              src={shevronLeft}
+              alt='Back to products'
+            />
+          </section>
+        </Link>
       </section>
     </main>
   );
