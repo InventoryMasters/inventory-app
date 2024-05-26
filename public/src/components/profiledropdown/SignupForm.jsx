945164857fe2bfc5847529955d20c92b2036d480
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-25 23:44:00 -0400

    refactor

--- public/src/components/profiledropdown/SignupForm.jsx
+++ public/src/components/profiledropdown/SignupForm.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
-import apiURL from '../../api';
+import apiURL from '../../api'
 
 export default function SignupForm({ setFormMode }) {
   const zodSignin = () =>
