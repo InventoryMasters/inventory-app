84d5d87d336a15fcb2e35e32978df9a3255cfc83
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-26 17:10:00 -0400

    refactor

--- public/src/components/products/allProducts/AllProducts.jsx
+++ public/src/components/products/allProducts/AllProducts.jsx
@@ -34,13 +34,13 @@ export default function AllProducts() {
   console.log({ products });
 
   return (
-    <section className='pt-20 font-encode'>
-      <section className='w-full banner-container flex flex-col justify-center items-center px-4'>
+    <section className='pt-20 font-encode '>
+      <section className='w-full banner-container flex flex-col justify-center items-center px-4 '>
         <img
           src='https://images.pexels.com/photos/7500307/pexels-photo-7500307.jpeg'
           className='w-full object-cover h-80 rounded-b-xl'
         />
-        <div className='relative text-white'>
+        <div className='relative text-white '>
           <h1 className='uppercase text-[7rem] absolute -translate-x-[45%] -translate-y-[140%] bg-transparent text-nowrap'>
             all products
           </h1>
@@ -51,24 +51,26 @@ export default function AllProducts() {
           <h1 className=' mt-10 uppercase font-medium text-lg text-center'>
             All Products
           </h1>
-          <SortFilter/>
+          <SortFilter />
         </div>
         <div className='grid grid-cols-3 gap-12 gap-y-12 mt-10 self-center'>
           {Array.isArray(products) && products.length ? (
             paginatedProducts.map((product) => (
               <Link to={`/products/${product.id}`}>
-              <div
-              key={product.id}
-              className='max-w-[350px] flex flex-col items-center text-center'
-              >
-              <img
-              className='w-full h-auto'
-              src={product.imageUrl}
-              alt={product.name}
-              />
-              <h2 className='uppercase font-regular pt-1'>{product.name}</h2>
-              <p className='font-thin'>${product.price}</p>
-              </div>
+                <div
+                  key={product.id}
+                  className='max-w-[350px] flex flex-col items-center text-center'
+                >
+                  <img
+                    className='w-full h-auto'
+                    src={product.imageUrl}
+                    alt={product.name}
+                  />
+                  <h2 className='uppercase font-regular pt-1'>
+                    {product.name}
+                  </h2>
+                  <p className='font-thin'>${product.price}</p>
+                </div>
               </Link>
             ))
           ) : (
