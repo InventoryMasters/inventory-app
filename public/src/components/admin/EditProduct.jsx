f0d4b85396bb526d16df39c852c03dd28ac835de
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-27 11:58:12 -0400

    add styling

--- public/src/components/admin/EditProduct.jsx
+++ public/src/components/admin/EditProduct.jsx
@@ -20,27 +20,33 @@ export default function EditProduct() {
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      try {
-        const response = await axios.get(`${apiURL}/items/${id}`);
-        setProduct(response.data);
-        setFormData({
-          name: response.data.name,
-          description: response.data.description,
-          price: response.data.price,
-          qty: response.data.qty,
-          imageUrl: response.data.imageUrl,
-          category: response.data.category.name,
-        });
-      } catch (err) {
-        console.error(`Error fetching product with ID: ${id} `, err.message);
-        throw err;
-      }
-    };
+useEffect(() => {
+  const fetchProduct = async () => {
+    try {
+      const response = await axios.get(`${apiURL}/items/${id}`);
+      setProduct(response.data);
+      setFormData({
+        name: response.data.name,
+        description: response.data.description,
+        price: response.data.price,
+        qty: response.data.qty,
+        imageUrl: response.data.imageUrl,
+        category:
+          response.data.category && response.data.category.length > 0
+            ? {
+                value: response.data.category[0].name,
+                label: response.data.category[0].name,
+              }
+            : '',
+      });
+    } catch (err) {
+      console.error(`Error fetching product with ID: ${id} `, err.message);
+      throw err;
+    }
+  };
 
-    fetchProduct();
-  }, [id]);
+  fetchProduct();
+}, [id]);
 
   useEffect(() => {
     const fetchAllCategories = async () => {
@@ -56,6 +62,8 @@ export default function EditProduct() {
     fetchAllCategories();
   }, []);
 
+  console.log({ categories });
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -67,10 +75,9 @@ export default function EditProduct() {
   const handleCategoryChange = (selectedOption) => {
     setFormData({
       ...formData,
-      category: selectedOption.value,
+      category: selectedOption,
     });
   };
-
   const submitEditProduct = async (e) => {
     e.preventDefault();
     try {
@@ -101,8 +108,8 @@ export default function EditProduct() {
 
   return (
     <section className='pt-28 font-encode text-3xl text-center font-semi-bold text-primary-dark-gray px-20'>
-      <h1>EDIT PRODUCT</h1>
-      <div className='flex justify-between'>
+      <h1 className='text-center'>EDIT PRODUCT</h1>
+      <div className='flex justify-around gap-44'>
         <div>
           <img
             src={product.imageUrl}
@@ -112,7 +119,7 @@ export default function EditProduct() {
         </div>
         <form
           onSubmit={submitEditProduct}
-          className='text-sm font-light border border-black -translate-x-[40%] w-[40vw] flex flex-col text-start'
+          className='text-sm font-light -translate-x-[40%] w-[40vw] flex flex-col text-start pt-10'
         >
           <label htmlFor='name'>name</label>
           <input
@@ -121,53 +128,65 @@ export default function EditProduct() {
             name='name'
             value={formData.name}
             onChange={handleInputChange}
-            className='border border-black w-[80%] text-black'
+            className='border border-black w-[80%] text-black py-2 rounded-md px-3'
           />
-          <label htmlFor='description'>description</label>
+          <label htmlFor='description' className='pt-5'>
+            description
+          </label>
           <textarea
             id='description'
             name='description'
             value={formData.description}
             onChange={handleInputChange}
-            className='border border-black w-[80%]'
+            className='border border-black w-[80%] h-28 text-black py-2 rounded-md px-3'
           />
-          <label htmlFor='price'>price</label>
+          <label htmlFor='price' className='pt-5'>
+            price
+          </label>
           <input
             type='number'
             id='price'
             name='price'
             value={formData.price}
             onChange={handleInputChange}
-            className='border border-black'
+            className='border border-black w-[80%] text-black py-2 rounded-md px-3'
           />
-          <label htmlFor='qty'>qty</label>
+          <label htmlFor='qty' className='pt-5'>
+            qty
+          </label>
           <input
             type='number'
             id='qty'
             name='qty'
             value={formData.qty}
             onChange={handleInputChange}
-            className='border border-black'
+            className='border border-black w-[80%] text-black py-2 rounded-md px-3'
           />
-          <label htmlFor='imageUrl'>Image URL</label>
+          <label htmlFor='imageUrl' className='pt-5'>
+            image URL
+          </label>
           <input
             type='text'
             id='imageUrl'
             name='imageUrl'
             value={formData.imageUrl}
             onChange={handleInputChange}
-            className='border border-black'
+            className='border border-black w-[80%] text-black py-2 rounded-md px-3'
           />
-          <h3>select category:</h3>
+          <h3 className='pt-5'>select category:</h3>
           <CreatableSelect
             options={categories.map((category) => ({
               value: category.name,
               label: category.name,
             }))}
-            value={{ value: formData.category, label: formData.category }}
+            value={formData.category}
             onChange={handleCategoryChange}
+            className=' w-[80%] text-black rounded-md '
           />
-          <button type='submit' className='border border-black'>
+          <button
+            type='submit'
+            className='border border-black w-[80%] text-black py-2 rounded-full px-3 mt-10 text-lg font-medium'
+          >
             EDIT PRODUCT
           </button>
         </form>
