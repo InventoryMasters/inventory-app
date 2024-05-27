dcbdd289a1a56adbd530c8f40fe44114ce3ffe5f
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-27 18:10:16 -0400

    had to remove validation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,71 +2,73 @@ const { User } = require('../models/index');
 const { validationResult } = require('express-validator');
 
 const getAllUsers = async (req, res) => {
-    try {
-        const users = await User.findAll();
-        if (!users) {
-            res.status(404).send("Error fetching users: Users not found");
-        } else {
-            res.status(200).json(users);
-        }
-    } catch (error) {
-        res.status(500).send(error);
+  try {
+    const users = await User.findAll();
+    if (!users) {
+      res.status(404).send('Error fetching users: Users not found');
+    } else {
+      res.status(200).json(users);
     }
-}
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
-const getUser = async (req, res) => {
-    try {
-        const userId = req.params.userId
-        const user = await User.findByPk(userId);
-        if (!user) {
-            res.status(404).send(`Error fetching user: User with ID ${userId} not found`);
-        } else {
-            res.status(200).json(user);
-        }
-    } catch (error) {
-        res.status(500).send(error);
+const getUser = async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
+    const user = await User.findByPk(userId);
+    if (!user) {
+      res
+        .status(404)
+        .send(`Error fetching user: User with ID ${userId} not found`);
+    } else {
+      res.status(200).json(user);
     }
-}
-
+  } catch (error) {
+    next(error);
+    res.status(500).send(error);
+  }
+};
 
 const updateUser = async (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+  try {
+    const newInfo = req.body;
+    const userId = req.params.userId;
+    const user = await User.findByPk(userId);
+    if (!user) {
+      res
+        .status(404)
+        .send(`Error updating user: User with ID ${userId} not found`);
+    } else {
+      await user.update(newInfo);
+      res.status(200).json(user);
     }
-    try {
-        const newInfo = req.body;
-        const userId = req.params.userId;
-        const user = await User.findByPk(userId);
-        if (!user) {
-            res.status(404).send(`Error updating user: User with ID ${userId} not found`);
-        } else {
-            await user.update(newInfo);
-            res.status(200).json(user);
-        }
-    } catch (error) {
-        next(error)
-    }
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
 const deleteUser = async (req, res) => {
-    try {
-        const userId = req.params.userId;
-        const user = await User.findByPk(userId);
-        if (!user) {
-            res.status(404).send(`Error deleting user: User with ID ${userId} not found`);
-        } else {
-            await user.destroy();
-            res.status(200).send("User removed!");
-        }
-    } catch (error) {
-        res.status(500).send(error);
+  try {
+    const userId = req.params.userId;
+    const user = await User.findByPk(userId);
+    if (!user) {
+      res
+        .status(404)
+        .send(`Error deleting user: User with ID ${userId} not found`);
+    } else {
+      await user.destroy();
+      res.status(200).send('User removed!');
     }
-}
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
 module.exports = {
-    getAllUsers,
-    getUser,
-    updateUser,
-    deleteUser
-}
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+};
