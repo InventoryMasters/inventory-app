c0f581193e4231e4bfc4e57df8c61de864a6d403
Stephanie <steph.frias01@me.com>
2024-05-28 10:48:43 -0400

    Update server to use dynamic port for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { sequelize } = require('./server/models');
 const app = require('./server/app');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const init = async () => {
   try {
