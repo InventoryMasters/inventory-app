2a314d11c2ab380e496f5f805b5400f6897a0f5b
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-28 04:23:17 -0400

    change to react hook form, add error handling

--- public/src/components/admin/EditProduct.jsx
+++ public/src/components/admin/EditProduct.jsx
@@ -1,52 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import apiURL from '../../api';
 import axios from 'axios';
 import CreatableSelect from 'react-select/creatable';
 import { useNavigate } from 'react-router';
+import { useForm, Controller } from 'react-hook-form';
 
 export default function EditProduct() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   const [categories, setCategories] = useState([]);
-  const [formData, setFormData] = useState({
-    name: '',
-    description: '',
-    price: '',
-    qty: '',
-    imageUrl: '',
-    category: '',
-  });
-
   const navigate = useNavigate();
+  const {
+    handleSubmit,
+    control,
+    register,
+    formState: { errors },
+    setValue,
+  } = useForm();
 
-useEffect(() => {
-  const fetchProduct = async () => {
-    try {
-      const response = await axios.get(`${apiURL}/items/${id}`);
-      setProduct(response.data);
-      setFormData({
-        name: response.data.name,
-        description: response.data.description,
-        price: response.data.price,
-        qty: response.data.qty,
-        imageUrl: response.data.imageUrl,
-        category:
-          response.data.category && response.data.category.length > 0
-            ? {
-                value: response.data.category[0].name,
-                label: response.data.category[0].name,
-              }
-            : '',
-      });
-    } catch (err) {
-      console.error(`Error fetching product with ID: ${id} `, err.message);
-      throw err;
-    }
-  };
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await axios.get(`${apiURL}/items/${id}`);
+        setProduct(response.data);
+        const { name, description, price, qty, imageUrl, category } =
+          response.data;
+        setValue('name', name);
+        setValue('description', description);
+        setValue('price', price);
+        setValue('qty', qty);
+        setValue('imageUrl', imageUrl);
+        setValue(
+          'category',
+          category ? { value: category.name, label: category.name } : null
+        );
+      } catch (err) {
+        console.error(`Error fetching product with ID: ${id} `, err.message);
+        throw err;
+      }
+    };
 
-  fetchProduct();
-}, [id]);
+    fetchProduct();
+  }, [id, setValue]);
 
   useEffect(() => {
     const fetchAllCategories = async () => {
@@ -62,32 +58,19 @@ useEffect(() => {
     fetchAllCategories();
   }, []);
 
-  console.log({ categories });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
   const handleCategoryChange = (selectedOption) => {
-    setFormData({
-      ...formData,
-      category: selectedOption,
-    });
+    setValue('category', selectedOption);
   };
-  const submitEditProduct = async (e) => {
-    e.preventDefault();
+
+  const onSubmit = async (data) => {
     try {
       const productFields = {
-        name: formData.name,
-        description: formData.description,
-        price: formData.price,
-        qty: formData.qty,
-        imageUrl: formData.imageUrl,
-        category: formData.category,
+        name: data.name,
+        description: data.description,
+        price: data.price,
+        qty: data.qty,
+        imageUrl: data.imageUrl,
+        category: data.category,
       };
 
       const prod = await axios.put(
@@ -118,71 +101,87 @@ useEffect(() => {
           />
         </div>
         <form
-          onSubmit={submitEditProduct}
+          onSubmit={handleSubmit(onSubmit)}
           className='text-sm font-light -translate-x-[40%] w-[40vw] flex flex-col text-start pt-10'
         >
           <label htmlFor='name'>name</label>
           <input
             type='text'
-            id='name'
-            name='name'
-            value={formData.name}
-            onChange={handleInputChange}
+            {...register('name', { required: 'Name is required' })}
             className='border border-black w-[80%] text-black py-2 rounded-md px-3'
           />
+          {errors.name && <p className='text-red-800'>{errors.name.message}</p>}
+
           <label htmlFor='description' className='pt-5'>
             description
           </label>
           <textarea
-            id='description'
-            name='description'
-            value={formData.description}
-            onChange={handleInputChange}
+            {...register('description', {
+              required: 'Description is required',
+            })}
             className='border border-black w-[80%] h-28 text-black py-2 rounded-md px-3'
           />
+          {errors.description && (
+            <p className='text-red-800'>{errors.description.message}</p>
+          )}
+
           <label htmlFor='price' className='pt-5'>
             price
           </label>
           <input
             type='number'
-            id='price'
-            name='price'
-            value={formData.price}
-            onChange={handleInputChange}
+            {...register('price', { required: 'Price is required' })}
             className='border border-black w-[80%] text-black py-2 rounded-md px-3'
           />
+          {errors.price && (
+            <p className='text-red-800'>{errors.price.message}</p>
+          )}
+
           <label htmlFor='qty' className='pt-5'>
             qty
           </label>
           <input
             type='number'
-            id='qty'
-            name='qty'
-            value={formData.qty}
-            onChange={handleInputChange}
+            {...register('qty', { required: 'Quantity is required' })}
             className='border border-black w-[80%] text-black py-2 rounded-md px-3'
           />
+          {errors.qty && <p className='text-red-800'>{errors.qty.message}</p>}
+
           <label htmlFor='imageUrl' className='pt-5'>
             image URL
           </label>
           <input
             type='text'
-            id='imageUrl'
-            name='imageUrl'
-            value={formData.imageUrl}
-            onChange={handleInputChange}
+            {...register('imageUrl', { required: 'Image URL is required' })}
             className='border border-black w-[80%] text-black py-2 rounded-md px-3'
           />
+          {errors.imageUrl && (
+            <p className='text-red-800'>{errors.imageUrl.message}</p>
+          )}
+
+          {/**
           <h3 className='pt-5'>select category:</h3>
-          <CreatableSelect
+          <Controller
+          control={control}
+          name='category'
+          rules={{ required: 'Category is required' }}
+          render={({ field }) => (
+            <CreatableSelect
+            {...field}
             options={categories.map((category) => ({
               value: category.name,
               label: category.name,
             }))}
-            value={formData.category}
+            className=' w-[80%] text-black rounded-md'
             onChange={handleCategoryChange}
-            className=' w-[80%] text-black rounded-md '
-          />
+            />
+            )}
+            />
+            {errors.category && (
+              <p className='text-red-800'>{errors.category.message}</p>
+              )}
+            */}
+
           <button
             type='submit'
             className='border border-black w-[80%] text-black py-2 rounded-full px-3 mt-10 text-lg font-medium'
