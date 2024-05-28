3528841873e5804b4b067b1d8a76ab116142364b
Bridget Galang <galanbri@gmail.com>
2024-05-27 22:38:53 -0700

    update port

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const { sequelize } = require('./server/models');
 const app = require('./server/app');
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
 const init = async () => {
   try {
     await sequelize.sync();
 
-    app.listen(PORT, () => {
-      console.log(`Server listening at http://localhost:${PORT}`);
-      console.log(PORT);
+    app.listen(port, () => {
+      console.log(`Server listening at http://localhost:${port}`);
+      console.log();
     });
   } catch (error) {
     console.error('Error starting server:', error);
